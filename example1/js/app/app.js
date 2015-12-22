d1926ec542657641c56c15d7a99ed7704043658f
Deivison Francisco <deivisondfs@gmail.com>
2015-12-22 16:22:01 -0300

    add list objects stock and add ng-repeat in index.html

--- example1/js/app/app.js
+++ example1/js/app/app.js
@@ -1,9 +1,3 @@
-
-var stock = {
-	name: "shirt",
-	price: 27,
-	description: "it's ok!"
-}
 var app = angular.module('store', []);
 
 app.controller('ListProducts', function(){
---
+++ example1/js/app/stock.js
@@ -0,0 +1,16 @@
+var stock = [
+	{
+		name: "shirt",
+		price: 27,
+		description: "it's ok!",
+		addCart:false,
+		soldOut:false
+	},
+	{
+		name: "T-shirt",
+		price: 300,
+		description: "it's ok!",
+		addCart:true,
+		soldOut:false
+	}
+]
