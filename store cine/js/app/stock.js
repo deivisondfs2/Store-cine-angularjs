bee313e5b0186ee1c7a899fd4fe44db52e2c7947
Deivison Francisco <deivisondfs@gmail.com>
2016-01-06 16:49:14 -0300

    add property ng-src in tag img and add many elements images in stock.js

--- store cine/js/app/stock.js
+++ store cine/js/app/stock.js
@@ -3,7 +3,12 @@ var stock = [
 		name: "Death Note Rewrite 2",
 		price: 27.99,
 		description: "Esse filme do anime Death Note, um resumo dos episodios 26 á 37 aonde começa o arco do Near.",
-		image: 'img/death_note.jpg',
+		images: [
+					{
+						full: 'img/death_note.jpg',
+						thumb: 'img/death_note.jpg'
+					}
+				],
 		reviews: 4,
 		link:'www.google.com',
 		category: 'action',
@@ -15,7 +20,12 @@ var stock = [
 		name: "Pixels",
 		price: 18.99,
 		description: "Em Pixels, quando seres intergalácticos interpretam um arquivo em vídeo com imagens de jogos de arcade clássicos.",		
-		image: 'img/Pixels-Online.jpg',
+		images: [
+					{
+						full: 'img/Pixels-Online.jpg',
+						thumb: 'img/Pixels-Online.jpg'
+					}
+				],
 		reviews: 6,
 		link:'www.google.com',
 		category: 'technology',
