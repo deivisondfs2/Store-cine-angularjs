02026f7905d8180ed43e7deca057373f38e5081e
Deivison Francisco <deivisondfs@gmail.com>
2016-01-07 13:44:44 -0300

    add new controller in view, add new logic for open tabs

--- store cine/js/app/app.js
+++ store cine/js/app/app.js
@@ -4,3 +4,17 @@ app.controller('ListMovies', function(){
 	this.product = stock;
 })
 
+
+app.controller('PainelTabsController', function(){
+	//set default 1
+	//this.tab = 1;
+
+	this.setTab = function(newTab){
+		this.tab = newTab;
+	}
+
+	this.isTabSelected = function(tab){
+		return this.tab == tab;
+	}
+
+})
--- store cine/js/app/stock.js
+++ store cine/js/app/stock.js
@@ -2,7 +2,11 @@ var stock = [
 	{
 		name: "Death Note Rewrite 2",
 		price: 27.99,
-		description: "Esse filme do anime Death Note, um resumo dos episodios 26 á 37 aonde começa o arco do Near.",
+		description: {
+						resume: "Esse filme do anime Death Note, um resumo dos episodios 26 á 37 aonde começa o arco do Near.",
+						descFull: "Uma série de mangá escrita por Tsugumi Ohba e ilustrada por Takeshi Obata, e que cuja adaptação para anime foi dirigida por Tetsurō Araki. A história centra-se em Light Yagami, um estudante do ensino médio que descobre um caderno sobrenatural chamado 'Death Note', no qual pode matar pessoas se os nomes forem escritos nele enquanto o portador visualizar mentalmente o rosto de alguém que quer assassinar.",
+					},
+					
 		images: [
 					{
 						full: 'img/death_note.jpg',
@@ -19,7 +23,11 @@ var stock = [
 	{
 		name: "Pixels",
 		price: 18.99,
-		description: "Em Pixels, quando seres intergalácticos interpretam um arquivo em vídeo com imagens de jogos de arcade clássicos.",		
+		description:{
+						resume: "Em Pixels, quando seres intergalácticos interpretam um arquivo em vídeo com imagens de jogos de arcade clássicos.",		
+						descFull: "Quando seres intergalácticos interpretam um arquivo em vídeo com imagens de jogos de arcade clássicos como uma declaração de guerra contra eles, eles atacam a Terra usando esses jogos como modelos para suas várias ofensivas. O presidente Will Cooper (Kevin James) busca ajuda de seu melhor amigo de infância Sam Brenner (Adam Sandler), um campeão de competições de vídeo-games nos anos 80 – e agora um instalador de home theater – para liderar uma equipe de jogadores veteranos (Peter Dinklage e Josh Gad), derrotar os alienígenas e salvar o planeta. Eles ainda vão contar com a ajuda da tenente-coronel Violet Von Patten (Michelle Monaghan), uma especialista em tecnologia que irá fornecer aos arcaders as armas exclusivas para lutar contra os aliens."
+					},
+					
 		images: [
 					{
 						full: 'img/Pixels-Online.jpg',
